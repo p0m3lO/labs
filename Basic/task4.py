617a23de05b4cadc133ec6e33244e0a29abbdc77
csanad.fekete <csanad.fekete@udemx.eu>
2023-05-25 00:22:41 +0200

    fixin some scripts

--- Basic/task4.py
+++ Basic/task4.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import re
 
 def check_permanent_hostname(target_hostname):
     hostname_file = "/etc/hostname"
@@ -11,10 +12,13 @@ def check_permanent_hostname(target_hostname):
 
         if hostname != target_hostname:
             return False
+        else:
+            return True
 
     except FileNotFoundError:
         print("Error: File not found. Make sure the script is run as root or with proper permissions.")
         return False
+
 
 def check_hostfile(target_hostname):
     hosts_file = "/etc/hosts"
@@ -23,23 +27,25 @@ def check_hostfile(target_hostname):
             hosts = f.readlines()
 
         for line in hosts:
-            if line.startswith('127.0.1.1'):
-                parts = line.strip().split()
-                if len(parts) > 1 and target_hostname in parts[1:]:
+            if line.startswith('127.0.0.1') or line.startswith('127.0.1.1'):
+                if re.search(r'\b' + re.escape(target_hostname) + r'\b', line):
                     return True
-                else:
-                    return False
 
-    except FileNotFoundError:
-        print("Error: File not found. Make sure the script is run as root or with proper permissions.")
         return False
 
+    except FileNotFoundError:
+        print(f"Error: {hosts_file} file not found.")
+        return False
+
+
 if __name__ == "__main__":
-    target_hostname = "gde-lab"
+    target_hostname = "gde-exam"
     result = check_permanent_hostname(target_hostname)
     hosts_result = check_hostfile(target_hostname)
-    if result:
-        print(f"The hostname is set permanently to '{target_hostname}'.")
+    if result and hosts_result:
+        print(f"The hostname is set permanently to '{target_hostname} and set in hosts file'.")
+        points = 8
+        print(f"Points: {points}")
     elif not hosts_result:
         print(f"The '{target_hostname}' is NOT set in hosts file")
         sys.exit(1)
